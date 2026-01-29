057f796ac30ae6bd0fac21817218d21bef1dc621
dhmyess <147247165+dhmyess@users.noreply.github.com>
2026-01-29 22:54:22 +0700

    Update test header and blockhash sample date

--- test.py
+++ test.py
@@ -9,8 +9,9 @@ def lev(nonce):
     return little_endian_value
 
 max_target = 0x00000000ffff0000000000000000000000000000000000000000000000000000
-# Test header
-hex_header = "0000003ed928bbba0d4904b547fd0a12f7985f00bd8af5c618bb0100000000000000000059833d60fa5d3b072a13ba1c15e78053c5dc31f36aaf09e5925c70a9de86348763ed7869a1fc0117"
+# Test header = version + prevhash + merkle_root + ntime + nbits
+# https://learnmeabitcoin.com/explorer/block/00000000000000000000132e777589264ca2ffc47319ea55f8cbf6f180a7293d
+hex_header = "00800220e8f33cff4a360027dbf6a0ffec523939b329a91ae58a01000000000000000000d67d87fef5331b8055007ca44082f38c63887a019352ca517332dc516fc2f6e5d2807b69a1fc0117"
 pool_target = 1
 print("Starting mining...")
 start = time.time()
@@ -33,4 +34,4 @@ else:
     Hashrate = rate/1000000
     print(f"Hashrate    : {Hashrate:.2f} MH/s")
     print(f"\nYou need {(0xffffffff/rate):.3f} second to complete nonce scan from 0 to {0xffffffff}")
-print("\nThis is sample blockhash from block 933,995 mined on 27 Jan 2026, 16:52:51")
+print("\nThis is sample blockhash from block 934,235 mined on 29 Jan 2026, 15:46:26")
